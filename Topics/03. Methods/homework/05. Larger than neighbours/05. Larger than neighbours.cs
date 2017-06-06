b2022d64a0b11815d64d5ded6cf8bbec3848d377
MonikaTzenova <monikatzenova@gmail.com>
2017-06-06 23:04:43 +0300

    Create 05. Larger than neighbours.cs

---
+++ Topics/03. Methods/homework/05. Larger than neighbours/05. Larger than neighbours.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+namespace _05.LargerThanNeighbours
+{
+    class Program
+    {
+        static int counter;
+
+        static void Main(string[] args)
+        { 
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            CheckNeighbours(n, arr);
+            Console.WriteLine(counter);
+        }
+
+        static void CheckNeighbours(int n, int[] arr)
+        {
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1])
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+}
